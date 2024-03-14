91eff79cf2499e00816506872911a1392a0eee3b
Lucas Martins de Oliveira <lucasm236954@gmail.com>
2024-03-14 01:17:05 -0300

    change colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import Toolbar from './components/home/Toolbar.tsx'
+import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <Toolbar></Toolbar>
-      </div>
+    <ThemeProvider defaultTheme="dark">
+      <Button variant="default">Button</Button>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/home/Toolbar.tsx
+++ src/components/home/Toolbar.tsx
@@ -1,17 +1,11 @@
 import "./Toolbar.css";
 import "../../index.css"
-import Drawer from '@mui/material/Drawer';
-import Buttona from '@mui/material/Button'
 import { Avatar, Button, Nav, Sidebar } from 'grommet';
 
 import {Help, Projects, Clock} from 'grommet-icons';
 
 export default function Toolbar() {
   return (
-   <>
-      <Drawer variant="permanent">
-        <Buttona variant="contained" sx={{color:'green'}}>Teste</Buttona>
-      </Drawer>
     <Sidebar
       background="var(--ctp-frappe-sky)"
       round="15px"
@@ -25,7 +19,6 @@ export default function Toolbar() {
         <Button icon={<Clock />} hoverIndicator />
       </Nav>
     </Sidebar>
-    </>
   );
 }
 
