007344715c0f7c356d517ca946b82fd79bab9831
Lucas Martins de Oliveira <lucasm236954@gmail.com>
2024-03-14 02:16:40 -0300

    add light theme

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-text hover:bg-hover hover:text-primary",
+        default: "bg-base text-text hover:bg-mauve hover:text-mantle",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
